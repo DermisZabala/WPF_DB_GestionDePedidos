083b09c03d3a2c2b343a369ae9f6724ca0755818
DermisZabala <dermiszabala@gmail.com>
2024-09-06 15:31:34 -0400

    Mostrando Datos

---
+++ Consultas/Consulta 1.sql
@@ -0,0 +1,11 @@
+﻿--Mediante la relacion entre las tablas Cliente y Pedido
+--muestra los clientes de Santo Domingo que han realizado pedidos
+SELECT * FROM Cliente INNER JOIN Pedido 
+ON Cliente.Id = Pedido.IdCliente WHERE poblacion = 'Santo Domingo';
+
+
+
+--Mediante la relacion entre las tablas Cliente y Pedido
+--muestra los clientes que ha pagado al 'Contado'
+SELECT * FROM Pedido INNER JOIN Cliente 
+ON Pedido.IdCliente = Cliente.Id WHERE formaPago = 'Contado';
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace WPF_DB_GestionPedidos
 {
@@ -21,11 +23,107 @@ namespace WPF_DB_GestionPedidos
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection miConexionSQL;
+
         public MainWindow()
         {
             InitializeComponent();
 
             string miConexion = ConfigurationManager.ConnectionStrings["WPF_DB_GestionPedidos.Properties.Settings.GestionPedidosConnectionString"].ConnectionString;
+            
+            miConexionSQL = new SqlConnection(miConexion);
+
+            MuestraClientes();
+
+            PedidosCompletos();
+        }
+
+        private void MuestraClientes()
+        {
+            miConexionSQL.Open();
+            
+            string consulta = "SELECT * FROM Cliente";
+
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, miConexionSQL);
+
+            DataTable tablaCliente = new DataTable();
+
+            adaptador.Fill(tablaCliente);
+                        
+            ListaCliente.DisplayMemberPath = "nombre";
+
+            ListaCliente.SelectedValuePath = "Id";
+            
+            ListaCliente.ItemsSource = tablaCliente.DefaultView;
+            
+            miConexionSQL.Close();
+        }
+
+        private void ListaCliente_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            MuestraPedidos();
+            
+        }
+
+        private void MuestraPedidos()
+        {
+            string consultaPEDIDO = "SELECT * FROM Pedido objP INNER JOIN Cliente objC ON " +
+                "objC.Id = objP.IdCliente WHERE objC.Id = @ClienteId";
+
+            SqlCommand sqlComando = new SqlCommand(consultaPEDIDO, miConexionSQL);
+
+            SqlDataAdapter miAdactador = new SqlDataAdapter(sqlComando);
+
+            DataTable TablaPedido = new DataTable();
+
+            // Añadir el parámetro el valor 
+            miAdactador.SelectCommand.Parameters.AddWithValue("@ClienteId", ListaCliente.SelectedValue);
+
+            miAdactador.Fill(TablaPedido);
+
+            ListaPedidos.DisplayMemberPath = "Id";
+            
+            ListaPedidos.SelectedValuePath = "IdCliente";
+            
+            ListaPedidos.ItemsSource = TablaPedido.DefaultView;
+            
+        }
+
+        private void PedidosCompletos()
+        {
+            string consulta = "SELECT CONCAT(Id, ' ', IdCliente, ' ', fechaPedido) AS INFOPEDIDO FROM pedido";
+
+            SqlDataAdapter miAdactadorSql = new SqlDataAdapter(consulta, miConexionSQL);
+
+            DataTable TablaPedidoCompleta = new DataTable();
+            
+                        
+            miAdactadorSql.Fill(TablaPedidoCompleta);
+
+            // Contar los pedidos
+            //contador.TryParse(Cantidad, out contador);
+
+            Cantidad.Content = TablaPedidoCompleta.Rows.Count;
+
+            
+
+            // Añadir una fila personalizada al final con el mensaje
+            //DataRow newRow = TablaPedidoCompleta.NewRow();
+            //newRow["INFOPEDIDO"] = $"Se han realizado {contador} pedidos";
+            //TablaPedidoCompleta.Rows.Add(newRow);
+
+            PedidoCompletos.DisplayMemberPath = $"INFOPEDIDO";            
+
+            PedidoCompletos.SelectedValuePath = "Id";
+
+            PedidoCompletos.ItemsSource = TablaPedidoCompleta.DefaultView;
+
+        }
+
+        private void EliminarPedido_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
